c6a9c697ce592ffaecc8b81ce1655a87525477ff
Frescoref <frescoref@gmail.com>
2024-03-31 14:21:56 +0300

    Fix: more

--- views/accounts/update_form.php
+++ views/accounts/update_form.php
@@ -7,13 +7,13 @@
         <div class="pe-0 pe-lg-2">
             <form method="post" action="<?php echo esc_url(add_query_arg('form', $args['object']->getId())); ?>">
                 <?php wp_nonce_field('wsklad-admin-accounts-update-save', '_wsklad-admin-nonce'); ?>
-                <div class="bg-white p-2 rounded-3 wsklad-toc-container section-border">
+                <div class="bg-white p-2 pt-0 rounded-3 wsklad-toc-container section-border">
                     <table class="form-table wsklad-admin-form-table">
                         <?php $args['object']->generateHtml($args['object']->getFields(), true); ?>
                     </table>
                 </div>
-                <p class="submit">
-                    <input type="submit" name="submit" id="submit" class="button button-primary" value="<?php _e('Save account', 'wsklad'); ?>">
+                <p class="submit mt-0">
+                    <input type="submit" name="submit" id="submit" class="button button-primary p-1 fs-6 px-3" value="<?php _e('Save account', 'wsklad'); ?>">
                 </p>
             </form>
         </div>
